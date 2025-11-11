38ea4dad191871e89eef1c20dcf7744912aa6395
toudefu <2696810816@qq.com>
2025-11-11 11:13:55 +0800

    fix: #define name

--- third_party/ascend/backend/flagtree_backend_specialization/include/triton/Dialect/Triton/IR/ascend_Dialect.h
+++ third_party/ascend/backend/flagtree_backend_specialization/include/triton/Dialect/Triton/IR/ascend_Dialect.h
@@ -1,6 +1,6 @@
 #ifndef ASCEND_TRITON_DIALECT_TRITON_IR_DIALECT_H_
 #define ASCEND_TRITON_DIALECT_TRITON_IR_DIALECT_H_
 
-#define FLAGTREE_SPEC_Dialect_Triton_IR_OpInterfaces_inc
+#define FLAGTREE_SPEC_Dialect_Triton_IR_OpInterfaces_head
 
 #endif // ASCEND_TRITON_DIALECT_TRITON_IR_DIALECT_H_
