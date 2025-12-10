f9664818e029afc9d726a00958e0acf7a179c8f0
yrl <2535184404@qq.com>
2025-12-10 17:03:36 +0800

    update

--- python/triton/_flagtree_tools.py
+++ python/triton/_flagtree_tools.py
@@ -6,15 +6,69 @@ import tempfile
 from io import StringIO
 import contextlib
 import ast
+import types
 import pandas as pd
+import pickle
+from dataclasses import dataclass
+from typing import Callable, Any
 import triton.runtime as runtime
+'''
+    Currently, the use of flagtree_do_bench is restricted, mainly including:
+
+    1. This method can only be used to test the kernel running time of triton and torch;
+
+    2. Arg fn, is either a direct single-test wrapper, such as
+        (1) def test():
+        (2). fn = op()
+
+'''
 
 
-def flagtree_do_bench(fn, warmup=10, rep=5, quantiles=None, return_mode="mean"):
+def flagtree_do_bench(fn, warmup=10, rep=5, quantiles=None, return_mode="mean") -> float:
     assert return_mode in ["mean", "min", "max", "sum"]
     bench = FlagtreeBench(current_fn=fn, warmup=warmup, rep=rep, quantiles=quantiles, return_mode=return_mode)
     bench.do_bench()
     return bench.results[return_mode]
+
+
+def check_ncu():
+    cmd = ["ncu", "--query-metrics"]
+    try:
+        subprocess.run(cmd, capture_output=True, check=True)
+        print("[INFO]: ncu check successfully")
+        return True
+    except Exception as err_msg:
+        print(f"\033[31m[Error] The inability to invoke ncu on this machine"
+              f"might be due to issues such as the absence of ncu, "
+              f"lack of permissions, or a version that is too low. Specifically \n{err_msg}\033[0m")
+        return False
+
+
+def run_warmup(_fn, warmup):
+    '''
+        Referred to triton.testing.do_bench
+    '''
+    di = runtime.driver.active.get_device_interface()
+    _fn()
+    di.synchronize()
+    cache = runtime.driver.active.get_empty_cache_for_benchmark()
+
+    # Estimate the runtime of the function
+    start_event = di.Event(enable_timing=True)
+    end_event = di.Event(enable_timing=True)
+    start_event.record()
+    for _ in range(5):
+        runtime.driver.active.clear_cache(cache)
+        _fn()
+    end_event.record()
+    di.synchronize()
+    estimate_ms = start_event.elapsed_time(end_event) / 5
+
+    # compute number of warmup and repeat
+    n_warmup = max(1, int(warmup / estimate_ms))
+
+    for _ in range(n_warmup):
+        _fn()
 
 
 '''
@@ -84,6 +138,148 @@ class IndentedBuffer:
         return ctx()
 
 
+@dataclass
+class FuncAttrs:
+    _globals: dict = None,
+    is_lambda: bool = False
+    is_torch_method: bool = False,
+    source: str = ''
+    ast_tree: any = None,
+    functor_source: str = ''
+    Argument_serialized: bool = False
+    Argument_serialized_path: str = ''
+
+
+class CodeGenerator:
+
+    @staticmethod
+    def gen_load_args_kwargs_method(script_code: IndentedBuffer, Trait: FuncAttrs):
+        script_code.writeline("def load_args_kwargs(filename):")
+        with script_code.indent():
+            script_code.writeline("import pickle")
+            script_code.writeline("with open(filename, 'rb') as f:")
+            with script_code.indent():
+                script_code.writeline("data = pickle.load(f)")
+                script_code.writeline("return data['args'], data['kwargs']")
+
+        script_code.writeline(f"args, kwargs = load_args_kwargs('{Trait.Argument_serialized_path}')")
+        script_code.writeline(f"{Trait.functor_source}(*args, **kwargs)")
+
+    @staticmethod
+    def _gen_import_and_path(script_code: IndentedBuffer, unpacked, path_mode='insert'):
+        sys_path_action_str = '0, '
+        if path_mode == 'insert':
+            script_code.writeline('import torch')
+            script_code.writeline('import os')
+            script_code.writeline('import sys')
+        else:
+            sys_path_action_str = ''
+        user_package_path = os.environ.get('BENCH_MODULE_PATH', '')
+        if user_package_path != '':
+            script_code.writeline(f"sys.path.{path_mode}({sys_path_action_str}'{user_package_path}')")
+
+        # create extra modules
+        if not unpacked:
+            return
+        else:
+            calls, modules, deps_path = unpacked
+        for path in deps_path:
+            if not os.path.isdir(path):
+                path = os.path.dirname(path)
+            script_code.writeline(f"sys.path.{path_mode}({sys_path_action_str}'{path}')")
+        if path_mode == 'insert':
+            for mod in modules:
+                script_code.writeline(f'import {mod}')
+            for call, mod in calls:
+                script_code.writeline(f"from {mod} import {call}")
+
+    @staticmethod
+    def _gen_lambda_source_code(script_code: IndentedBuffer, Trait: FuncAttrs):
+        if Trait.is_torch_method:
+            CodeGenerator._gen_torch_using_lambda_code(script_code, Trait)
+        else:
+            CodeGenerator._gen_triton_code(script_code)
+
+    @staticmethod
+    def _gen_torch_using_lambda_code(script_code: IndentedBuffer, Trait: FuncAttrs):
+        if Trait.Argument_serialized:
+            CodeGenerator.gen_load_args_kwargs_method(script_code, Trait)
+        else:
+            script_code.writeline(f"{Trait.functor_source}()")
+
+    @staticmethod
+    def _gen_triton_code():
+        ...
+
+
+class FunctionExtractor:
+
+    def __init__(self, fn: Callable[..., Any] = None):
+        if fn.__name__ == "<lambda>":
+            self.trait = self.analyse_lambda(fn)
+        else:
+            self.trait = self.analyse_general(fn)
+
+    def args_serialization(self, *args, **kwargs):
+        packed_datas = tempfile.NamedTemporaryFile(delete=False, suffix=".pkl")
+        with open(packed_datas.name, "wb") as f:
+            pickle.dump({"args": args, "kwargs": kwargs}, f)
+        return packed_datas.name
+
+    def analyse_lambda(self, fn):
+        is_lambda = True
+        if not hasattr(fn, '__closure__'):
+            return
+
+        def handlecase_single(closure_package, fn):
+            ...
+
+        def handlecase_both(closure_package, fn):
+            ...
+
+        def handlecase_all(closure_package, fn):
+            source = inspect.getsource(fn)
+            args, kwargs = closure_package[0].cell_contents, closure_package[1].cell_contents
+            serialized_path = self.args_serialization(*args, **kwargs)
+            functor = closure_package[-1].cell_contents
+
+            # Case 1: method_descriptor → Tensor-level method
+            # e.g., torch.Tensor.addmm
+            if inspect.ismethoddescriptor(functor) or hasattr(functor, "__objclass__"):
+                functor_mod_name = functor.__objclass__.__module__.replace("torch._C", "torch")
+                functor_source = f"{functor_mod_name}.{functor.__name__}"
+
+            # Case 2: bound method (x.addmm)
+            if inspect.ismethod(functor):
+                cls = functor.__self__.__class__
+                functor_source = f"{cls.__module__}.{cls.__qualname__}.{functor.__name__}"
+
+            if isinstance(functor, (types.BuiltinFunctionType, types.BuiltinMethodType)):
+                functor_source = f"torch.{functor.__name__}"
+
+            is_torch_method = 'torch' in functor_source
+            return FuncAttrs(_globals=fn.__globals__, source=inspect.getsource(fn), is_torch_method=is_torch_method,
+                             Argument_serialized=True, Argument_serialized_path=serialized_path,
+                             functor_source=functor_source, ast_tree=ast.parse(textwrap.dedent(source)),
+                             is_lambda=is_lambda)
+
+        casehandlers = [handlecase_single, handlecase_both, handlecase_all]
+        casehandlers_mapping = {}
+        for case_idx, handler in enumerate(casehandlers):
+            casehandlers_mapping[case_idx] = handler
+
+        handle_closure = lambda _type, _closure_package, _fn: casehandlers_mapping[_type](_closure_package, _fn)
+        closure_package = fn.__closure__
+        closure_package_len = len(closure_package) - 1
+
+        return handle_closure(closure_package_len, closure_package, fn)
+
+    def analyse_general(self, fn):
+        source = inspect.getsource(fn)
+        return FuncAttrs(source=source, ast_tree=ast.parse(textwrap.dedent(source)), is_lambda=False,
+                         _globals=fn.__globals__)
+
+
 '''
     FlagtreeBench using ncu to measure performance
 '''
@@ -92,7 +288,7 @@ class IndentedBuffer:
 class FlagtreeBench:
 
     def __init__(self, current_fn, warmup=10, rep=5, quantiles=None, return_mode="mean", metrics='gpu__time_duration'):
-        if FlagtreeBench.check_ncu():
+        if check_ncu():
             self._current_fn = current_fn
             self.metrics = metrics
             self.warmup = warmup
@@ -100,21 +296,8 @@ class FlagtreeBench:
             self.quantiles = quantiles
             self.return_mode = return_mode
             self.triton_funcs = []
-            self._get_package_path()
             self._create_temp_file()
-
-    @staticmethod
-    def check_ncu():
-        cmd = ["ncu", "--query-metrics"]
-        try:
-            subprocess.run(cmd, capture_output=True, check=True)
-            print("[INFO]: ncu check successfully")
-            return True
-        except Exception as err_msg:
-            print(f"\033[31m[Error] The inability to invoke ncu on this machine"
-                  f"might be due to issues such as the absence of ncu, "
-                  f"lack of permissions, or a version that is too low. Specifically \n{err_msg}\033[0m")
-            return False
+            print(self.python_exec.name, self.out_csv.name)
 
     @staticmethod
     def gather_triton_jit_kernel(mod):
@@ -140,10 +323,10 @@ class FlagtreeBench:
         return 'site-packages' in mod.__file__
 
     def _get_current_function_used_mod(self, _fn=None):
-        _fn = _fn or self._current_fn
-        func_global_dict = _fn.__globals__
-        source = inspect.getsource(_fn)
-        tree = ast.parse(source)
+        attrs = self.fn_trait
+        if attrs.is_lambda:
+            return None
+        func_global_dict = attrs._globals
         modules = set()
         calls = set()
         deps_path = set()
@@ -164,13 +347,14 @@ class FlagtreeBench:
             def visit_Call(self, node):
                 if isinstance(node.func, ast.Name):
                     fun_name = node.func.id
-                    func_instance = func_global_dict[fun_name]
-                    mod_instance = __import__(func_instance.__module__)
-                    triton_jit_kernels.update(FlagtreeBench.gather_triton_jit_kernel(mod_instance))
-                    if hasattr(mod_instance, '__file__'):
-                        mod_dir_path = os.path.dirname(mod_instance.__file__)
-                        deps_path.add(mod_dir_path)
-                    calls.add((fun_name, mod_instance.__name__))
+                    if fun_name in func_global_dict:
+                        func_instance = func_global_dict[fun_name]
+                        mod_instance = __import__(func_instance.__module__)
+                        triton_jit_kernels.update(FlagtreeBench.gather_triton_jit_kernel(mod_instance))
+                        if hasattr(mod_instance, '__file__'):
+                            mod_dir_path = os.path.dirname(mod_instance.__file__)
+                            deps_path.add(mod_dir_path)
+                        calls.add((fun_name, mod_instance.__name__))
 
                 elif isinstance(node.func, ast.Attribute):
                     fun_name = node.func.attr
@@ -180,11 +364,8 @@ class FlagtreeBench:
                         triton_jit_kernels.update(FlagtreeBench.gather_triton_jit_kernel(mod_instance))
                 self.generic_visit(node)
 
-        Visitor().visit(tree)
+        Visitor().visit(attrs.ast_tree)
         return (calls, modules, deps_path)
-
-    def _get_package_path(self):
-        self.user_package_path = os.environ.get('BENCH_MODULE_PATH', '')
 
     def _create_temp_file(self):
         self.python_exec = tempfile.NamedTemporaryFile(delete=False, suffix=".py")
@@ -198,7 +379,6 @@ class FlagtreeBench:
             f.write(script)
 
     def _exec(self):
-        runtime.driver.active.clear_cache(self.bench_cache)
         cmd = [
             "ncu",
             "--metrics",
@@ -223,10 +403,13 @@ class FlagtreeBench:
 
     def _get_index(self):
         indexs = ['avg', 'max', 'min', 'sum']
-        patterns = "at::|std::"
+        patterns = "at::|std::|void"
         index_dict = dict.fromkeys(indexs, 0)
         df = pd.read_csv(self.out_csv.name)
-        metric_values = df[~df["Kernel Name"].str.contains(patterns, regex=True)][["Metric Name", "Metric Value"]]
+        if self.fn_trait.is_torch_method:
+            metric_values = df[df["Kernel Name"].str.contains(patterns, regex=True)][["Metric Name", "Metric Value"]]
+        else:
+            metric_values = df[~df["Kernel Name"].str.contains(patterns, regex=True)][["Metric Name", "Metric Value"]]
         for _, row in metric_values.iterrows():
             metric_name = str(row['Metric Name']).split('.')[-1]
             gpu_time = float(row['Metric Value']) / 1e6
@@ -234,76 +417,32 @@ class FlagtreeBench:
         index_dict['mean'] = index_dict['avg']
         return index_dict
 
-    def _gen_import_and_path(self, script_code: IndentedBuffer, path_mode='insert'):
-        calls, modules, deps_path = self._get_current_function_used_mod()
-        sys_path_action_str = '0, '
-        if path_mode == 'insert':
-            script_code.writeline('import torch')
-            script_code.writeline('import os')
-            script_code.writeline('import sys')
-        else:
-            sys_path_action_str = ''
-        if self.user_package_path != '':
-            script_code.writeline(f"sys.path.{path_mode}({sys_path_action_str}'{self.user_package_path}')")
-        for path in deps_path:
-            if not os.path.isdir(path):
-                path = os.path.dirname(path)
-            script_code.writeline(f"sys.path.{path_mode}({sys_path_action_str}'{path}')")
-        if path_mode == 'insert':
-            for mod in modules:
-                script_code.writeline(f'import {mod}')
-            for call, mod in calls:
-                script_code.writeline(f"from {mod} import {call}")
-
     def _generate_script(self, _fn=None):
         _fn = _fn or self._current_fn
         fn_src_code_string = textwrap.dedent(inspect.getsource(_fn))
         script_code = IndentedBuffer()
-        self._gen_import_and_path(script_code, path_mode='insert')
+        unpacked = self._get_current_function_used_mod()
+        CodeGenerator._gen_import_and_path(script_code, unpacked, path_mode='insert')
 
-        script_code.writeline(fn_src_code_string)
-        script_code.writeline(f'{_fn.__name__}()')
+        if self.fn_trait.is_lambda:
+            CodeGenerator._gen_lambda_source_code(script_code, self.fn_trait)
+        else:
+            script_code.writeline(fn_src_code_string)
+            script_code.writeline(f'{_fn.__name__}()')
         script_code.writeline("torch.cuda.synchronize()")
 
-        self._gen_import_and_path(script_code, path_mode='remove')
+        CodeGenerator._gen_import_and_path(script_code, unpacked, path_mode='remove')
+
         self.script = script_code.getvalue()
         self._write_script(self.script)
 
-    def _pre_operation(self, _fn=None):
-        '''
-            Referred to triton.testing.do_bench
-        '''
-        _fn = _fn or self._current_fn
-        di = runtime.driver.active.get_device_interface()
-        _fn()
-        di.synchronize()
-        cache = runtime.driver.active.get_empty_cache_for_benchmark()
-
-        # Estimate the runtime of the function
-        start_event = di.Event(enable_timing=True)
-        end_event = di.Event(enable_timing=True)
-        start_event.record()
-        for _ in range(5):
-            runtime.driver.active.clear_cache(cache)
-            _fn()
-        end_event.record()
-        di.synchronize()
-        estimate_ms = start_event.elapsed_time(end_event) / 5
-
-        # compute number of warmup and repeat
-        n_warmup = max(1, int(self.warmup / estimate_ms))
-
-        self.bench_cache = cache
-        for _ in range(n_warmup):
-            _fn()
-
-    def do_bench(self):
+    def do_bench(self) -> float:
         '''
             Measure the GPU kernel time of fn() using ncu.
             Generate a temporary Python file and then run it with 'ncu'.
         '''
-        self.used_mods = self._get_current_function_used_mod()
+        self.fn_trait = FunctionExtractor(self._current_fn).trait
         self._generate_script()
-        self._pre_operation()
+        run_warmup(self._current_fn, self.warmup)
         self._exec()
         self.results = self._get_index()
