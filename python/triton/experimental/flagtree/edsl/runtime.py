a5401a54df03e789a572eb26bb40ada08dda4318
SabYic <sunwenjia04@163.com>
2025-12-22 15:07:01 +0800

    modified:   ../triton/experimental/flagtree/edsl/runtime.py

--- python/triton/experimental/flagtree/edsl/runtime.py
+++ python/triton/experimental/flagtree/edsl/runtime.py
@@ -1,12 +1,12 @@
 from .mlir import EdslMLIRJITFunction
-
+from typing import Any, Dict, Final, List, Optional
 registry = {"mlir": EdslMLIRJITFunction}
 
 
-def dialect(*, name: str):
+def dialect(*, name: str, pipeline: Optional[List[str]] | None = None):
 
     def decorator(fn):
-        edsl = registry[name](fn)
+        edsl = registry[name](fn, pipeline=pipeline)
         setattr(edsl, "__triton_builtin__", True)
         return edsl
 
